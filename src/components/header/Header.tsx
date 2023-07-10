c41a526bac0ebe66a6aaef352f1c4b2d076d0d48
Warrant TSAI <warrant@cleanstormwater.com.au>
2023-07-10 12:21:27 +1000

    feat: use styled-component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+// modules
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+
+function Header() {
+  return (
+    <AppBar position="static" style={{ width: "100%" }}>
+      <Toolbar></Toolbar>
+    </AppBar>
+  );
+}
+
+export default Header;
--- src/pages/layout/Layout.tsx
+++ src/pages/layout/Layout.tsx
@@ -1,12 +1,42 @@
+// modules
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+// components
+import Header from "../../components/header/Header";
 
 function Layout() {
+  const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    min-width: 100vw;
+  `;
+
+  const HeaderContainer = styled.div`
+    background-color: #f1f1f1;
+    padding: 20px;
+  `;
+
+  const OutletContainer = styled.div`
+    flex-grow: 1;
+    padding: 20px;
+  `;
+
+  const FooterContainer = styled.div`
+    background-color: #f1f1f1;
+    padding: 20px;
+  `;
+
   return (
-    <div>
-      123
-      <Outlet /> {/* Renders the nested routes */}
-      456
-    </div>
+    <Container>
+      <HeaderContainer>
+        <Header />
+      </HeaderContainer>
+      <OutletContainer>
+        <Outlet />
+      </OutletContainer>
+      <FooterContainer>{/* <Footer /> */}456</FooterContainer>
+    </Container>
   );
 }
 
