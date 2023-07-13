ebc484dfe45c517f21d7685d046fadb9fcf15adc
Warrant TSAI <warrant@cleanstormwater.com.au>
2023-07-13 20:40:33 +1000

    feat: import comopnents to routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import "./App.css";
 import Home from "./pages/home/Home";
 import Layout from "./pages/layout/Layout";
 import AboutMe from "./pages/about-me/AboutMe";
+import MyMajority from "./pages/my-majority/MyMajority";
+import WorkingExperience from "./pages/working-experience/WorkingExperience";
+import OtherExperience from "./pages/other-experience/OtherExperience";
+import MyHobbies from "./pages/my-hobbies/MyHobbies";
 import ContactWithMe from "./pages/contact-with-me/ContactWithMe";
 import NotFoundPage from "./pages/not-found-page/NotFoundPage";
 
@@ -16,10 +20,10 @@ function App() {
       <Route path="*" element={<Home />} />
       <Route path="/*" element={<Layout />}>
         <Route path="about-me" element={<AboutMe />} />
-        <Route path="my-majority" element={<AboutMe />} />
-        <Route path="working-experience" element={<AboutMe />} />
-        <Route path="other-experience" element={<AboutMe />} />
-        <Route path="my-hobbies" element={<AboutMe />} />
+        <Route path="my-majority" element={<MyMajority />} />
+        <Route path="working-experience" element={<WorkingExperience />} />
+        <Route path="other-experience" element={<OtherExperience />} />
+        <Route path="my-hobbies" element={<MyHobbies />} />
         <Route path="contact-with-me" element={<ContactWithMe />} />
       </Route>
       <Route path="/*" element={<NotFoundPage />} />
