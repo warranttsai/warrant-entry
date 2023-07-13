59167ef12271f95b279ffd5857f626cdfd3b89c3
Warrant TSAI <warrant@cleanstormwater.com.au>
2023-07-13 16:29:34 +1000

    feat: add test cases to home

--- src/__test__/Home.test.tsx
+++ src/__test__/Home.test.tsx
@@ -6,41 +6,41 @@ import "@testing-library/jest-dom";
 import Home from "../pages/home/Home.tsx";
 
 describe("<Home />", () => {
-  test("Home page was rendered", () => {
-    render(<Home />);
-    const checkContent = screen.getByText("this is home");
-    expect(checkContent).toBeTruthy();
-  });
   // context
-  test("has context 'Welcom to Warrant's website! Hope you enjoy the time browsing herer!'", () => {
+  test("has context 'Welcome to Warrant's website! Hope you enjoy the time browsing herer!'", () => {
     render(<Home />);
-    const checkContent = screen.getByText("this is home");
+    const checkContent = screen.getByTestId("home-title");
     expect(checkContent).toBeTruthy();
   });
   // buttons
   test("has button with context 'About Me'", () => {
     render(<Home />);
-    const checkContent = screen.getByText("this is home");
-    expect(checkContent).toBeTruthy();
+    const buttonElement = screen.getByTestId("about-me");
+    expect(buttonElement.textContent).toContain("About Me");
   });
-  test("has button with context 'Majority In School'", () => {
+  test("has button with context 'My Majority'", () => {
     render(<Home />);
-    const checkContent = screen.getByText("this is home");
-    expect(checkContent).toBeTruthy();
+    const buttonElement = screen.getByTestId("my-majority");
+    expect(buttonElement.textContent).toContain("My Majority");
+  });
+  test("has button with context 'Working Experience'", () => {
+    render(<Home />);
+    const buttonElement = screen.getByTestId("working-experience");
+    expect(buttonElement.textContent).toContain("Working Experience");
   });
   test("has button with context 'Other Experience'", () => {
     render(<Home />);
-    const checkContent = screen.getByText("this is home");
-    expect(checkContent).toBeTruthy();
+    const buttonElement = screen.getByTestId("other-experience");
+    expect(buttonElement.textContent).toContain("Other Experience");
   });
   test("has button with context 'My Hobbies'", () => {
     render(<Home />);
-    const checkContent = screen.getByText("this is home");
-    expect(checkContent).toBeTruthy();
+    const buttonElement = screen.getByTestId("my-hobbies");
+    expect(buttonElement.textContent).toContain("My Hobbies");
   });
   test("has button with context 'Contact With Me'", () => {
     render(<Home />);
-    const checkContent = screen.getByText("this is home");
-    expect(checkContent).toBeTruthy();
+    const buttonElement = screen.getByTestId("contact-with-me");
+    expect(buttonElement.textContent).toContain("Contact With Me");
   });
 });
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,15 @@
 function Home() {
   return (
     <>
-      <div id="test">this is home</div>
+      <div data-testid="home-title">
+        Welcome to Warrant's website! Hope you enjoy the time browsing herer!
+      </div>
+      <button data-testid="about-me">About Me</button>
+      <button data-testid="my-majority">My Majority</button>
+      <button data-testid="working-experience">Working Experience</button>
+      <button data-testid="other-experience">Other Experience</button>
+      <button data-testid="my-hobbies">My Hobbies</button>
+      <button data-testid="contact-with-me">Contact With Me</button>
     </>
   );
 }
