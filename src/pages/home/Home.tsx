6508ed820564284c9e8550d277e61a1b8c4b84c2
Warrant TSAI <warrant@cleanstormwater.com.au>
2023-07-13 17:02:31 +1000

    feat: add styled component for home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,38 +1,55 @@
+// modules
 import { Button } from "@mui/material";
+import styled from "styled-components";
 
 function Home() {
+  const Container = styled.div`
+    padding-inline: 300px;
+  `;
+  const TitleStyle = styled.h1`
+    color: #b48608;
+    font-family: "Droid serif", serif;
+    font-size: 60px;
+    font-weight: 400;
+    font-style: italic;
+    line-height: 44px;
+    margin: 0 0 40px 0;
+    text-align: center;
+  `;
+  const ButtonsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding-inline: 300px;
+    gap: 5px;
+  `;
+
   return (
-    <>
-      <div data-testid="home-title">
-        Welcome to Warrant's website! Hope you enjoy the time browsing herer!
-      </div>
-      <Button variant="contained" data-testid="about-me">
-        About Me
-      </Button>
-      <Button variant="contained" data-testid="my-majority">
-        My Majority
-      </Button>
-      <Button variant="contained" data-testid="working-experience">
-        Working Experience
-      </Button>
-      <Button
-        variant="contained"
-        data-testid="other-experience"
-        style={{ backgroundColor: "white", color: "black" }}
-      >
-        Other Experience
-      </Button>
-      <Button
-        variant="contained"
-        data-testid="my-hobbies"
-        style={{ backgroundColor: "white", color: "black" }}
-      >
-        My Hobbies
-      </Button>
-      <Button variant="contained" data-testid="contact-with-me">
-        Contact With Me
-      </Button>
-    </>
+    <Container>
+      <TitleStyle data-testid="home-title">
+        Greetings! <br />
+        Welcome to Warrant's website!
+      </TitleStyle>
+      <ButtonsContainer>
+        <Button variant="contained" data-testid="about-me">
+          About Me
+        </Button>
+        <Button variant="contained" data-testid="my-majority">
+          My Majority
+        </Button>
+        <Button variant="contained" data-testid="working-experience">
+          Working Experience
+        </Button>
+        <Button variant="contained" data-testid="other-experience">
+          Other Experience
+        </Button>
+        <Button variant="contained" data-testid="my-hobbies">
+          My Hobbies
+        </Button>
+        <Button variant="contained" data-testid="contact-with-me">
+          Contact With Me
+        </Button>
+      </ButtonsContainer>
+    </Container>
   );
 }
 
