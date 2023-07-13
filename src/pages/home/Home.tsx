c591b9c430e2af5336b3ca0565f1913e0daf5213
Warrant TSAI <warrant@cleanstormwater.com.au>
2023-07-13 16:37:02 +1000

    feat: use material ui for button

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,15 +1,37 @@
+import { Button } from "@mui/material";
+
 function Home() {
   return (
     <>
       <div data-testid="home-title">
         Welcome to Warrant's website! Hope you enjoy the time browsing herer!
       </div>
-      <button data-testid="about-me">About Me</button>
-      <button data-testid="my-majority">My Majority</button>
-      <button data-testid="working-experience">Working Experience</button>
-      <button data-testid="other-experience">Other Experience</button>
-      <button data-testid="my-hobbies">My Hobbies</button>
-      <button data-testid="contact-with-me">Contact With Me</button>
+      <Button variant="contained" data-testid="about-me">
+        About Me
+      </Button>
+      <Button variant="contained" data-testid="my-majority">
+        My Majority
+      </Button>
+      <Button variant="contained" data-testid="working-experience">
+        Working Experience
+      </Button>
+      <Button
+        variant="contained"
+        data-testid="other-experience"
+        style={{ backgroundColor: "white", color: "black" }}
+      >
+        Other Experience
+      </Button>
+      <Button
+        variant="contained"
+        data-testid="my-hobbies"
+        style={{ backgroundColor: "white", color: "black" }}
+      >
+        My Hobbies
+      </Button>
+      <Button variant="contained" data-testid="contact-with-me">
+        Contact With Me
+      </Button>
     </>
   );
 }
