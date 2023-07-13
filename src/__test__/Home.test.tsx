e3b071ec9af68a9b0c5466335058863eb0e10754
Warrant TSAI <warrant@cleanstormwater.com.au>
2023-07-13 22:47:46 +1000

    fix: use router to wrap the testing render

--- src/__test__/Home.test.tsx
+++ src/__test__/Home.test.tsx
@@ -2,44 +2,73 @@
 import { describe, test, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 // components
 import Home from "../pages/home/Home.tsx";
 
 describe("<Home />", () => {
   // context
   test("has context 'Welcome to Warrant's website! Hope you enjoy the time browsing herer!'", () => {
-    render(<Home />);
+    render(
+      <Router>
+        <Home />
+      </Router>
+    );
     const checkContent = screen.getByTestId("home-title");
     expect(checkContent).toBeTruthy();
   });
   // buttons
   test("has button with context 'About Me'", () => {
-    render(<Home />);
+    render(
+      <Router>
+        <Home />
+      </Router>
+    );
     const buttonElement = screen.getByTestId("about-me");
     expect(buttonElement.textContent).toContain("About Me");
   });
   test("has button with context 'My Majority'", () => {
-    render(<Home />);
+    render(
+      <Router>
+        <Home />
+      </Router>
+    );
     const buttonElement = screen.getByTestId("my-majority");
     expect(buttonElement.textContent).toContain("My Majority");
   });
   test("has button with context 'Working Experience'", () => {
-    render(<Home />);
+    render(
+      <Router>
+        <Home />
+      </Router>
+    );
     const buttonElement = screen.getByTestId("working-experience");
     expect(buttonElement.textContent).toContain("Working Experience");
   });
   test("has button with context 'Other Experience'", () => {
-    render(<Home />);
+    render(
+      <Router>
+        <Home />
+      </Router>
+    );
     const buttonElement = screen.getByTestId("other-experience");
     expect(buttonElement.textContent).toContain("Other Experience");
   });
   test("has button with context 'My Hobbies'", () => {
-    render(<Home />);
+    render(
+      <Router>
+        <Home />
+      </Router>
+    );
     const buttonElement = screen.getByTestId("my-hobbies");
     expect(buttonElement.textContent).toContain("My Hobbies");
   });
   test("has button with context 'Contact With Me'", () => {
-    render(<Home />);
+    render(
+      <Router>
+        <Home />
+      </Router>
+    );
     const buttonElement = screen.getByTestId("contact-with-me");
     expect(buttonElement.textContent).toContain("Contact With Me");
   });
