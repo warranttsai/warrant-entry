f12012185bcc79da8320e4c7ee4fb5bc22d38aa4
Warrant TSAI <warrant@cleanstormwater.com.au>
2023-07-20 14:44:57 +1000

    feat: create learning path test cases and basic component

--- src/pages/learning-path/LearningPath.tsx
+++ src/pages/learning-path/LearningPath.tsx
@@ -1,7 +1,46 @@
+// modules
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+// styled-components
+import { BlackCenterTitle } from "../../components/styled-component/Title";
+
 export default function LearningPath() {
+  const softwareEngineer = [{ id: 1, content: "test" }];
   return (
     <>
-      <div>123</div>
+      <div data-testid="software-engineering" style={{ minHeight: "40vh" }}>
+        <BlackCenterTitle>
+          👨‍💻 Software Engineering/Computer Science
+        </BlackCenterTitle>
+        <div
+          style={{ display: "flex", flexDirection: "row", overflow: "auto" }}
+        >
+          {softwareEngineer.map((item: { id: number; content: string }) => {
+            return (
+              <Card key={item.id} sx={{ minWidth: 275 }}>
+                <CardContent>
+                  <Typography
+                    sx={{ fontSize: 14 }}
+                    color="text.secondary"
+                    gutterBottom
+                  >
+                    {item.content}
+                  </Typography>
+                </CardContent>
+              </Card>
+            );
+          })}
+        </div>
+      </div>
+      <div
+        data-testid="street-dancing"
+        style={{ background: "#D3D3D3", minHeight: "40vh" }}
+      >
+        <BlackCenterTitle style={{ marginTop: "100px", paddingTop: "30px" }}>
+          🕺 Street Dancing
+        </BlackCenterTitle>
+      </div>
     </>
   );
 }
