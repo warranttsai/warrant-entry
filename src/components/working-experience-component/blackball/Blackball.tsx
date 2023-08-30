bc37893a2b518ba09beef6d1e032a227c8c7c205
Warrant TSAI <warrant@cleanstormwater.com.au>
2023-08-30 14:44:35 +1000

    feat: create  blackball working experience component

---
+++ src/components/working-experience-component/blackball/Blackball.tsx
@@ -0,0 +1,67 @@
+// modules
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+// styled-components
+import { Flex1 } from "../../styled-component/Container";
+import { BrownCenterTitle } from "../../styled-component/Title";
+
+export default function Blackball() {
+  return (
+    <Flex1
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "left",
+      }}
+    >
+      <Card
+        sx={{
+          background: "white",
+          border: "5px solid rgba(3, 102, 214, 0.3)",
+          width: 800,
+        }}
+      >
+        <CardContent
+          style={{
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <BrownCenterTitle
+            style={{
+              fontSize: "30px",
+              flex: 1,
+              marginBlock: "10px",
+              textAlign: "left",
+            }}
+          >
+            Blackball Chinatown - from January 2021 to May 2022
+          </BrownCenterTitle>
+          <span style={{ flex: 1, fontWeight: 700 }}>
+            Food and beverage attendant
+          </span>
+          <span style={{ flex: 1 }}>
+            Since my arrival in Melbourne, Australia, Blackball has been my
+            initial workplace experience. It presented a fast-paced,
+            teamwork-focused environment that demanded adept time management
+            skills. During my tenure at the milk tea shop, I acquired the
+            ability to motivate myself and effectively incorporate necessary
+            periods of relaxation amidst my responsibilities.
+          </span>
+          <ul style={{ flex: 1 }}>
+            <li>Customer Service</li>
+            <li>Opening/ Closing Shift</li>
+            <li>Kitchen Hand/ Dish Hand</li>
+            <li>Stock-taking</li>
+            <li>
+              Experienced in fast-paced environment in public holiday and long
+              shift of open to close shift.
+            </li>
+          </ul>
+        </CardContent>
+      </Card>
+    </Flex1>
+  );
+}
--- src/pages/working-experience/WorkingExperience.tsx
+++ src/pages/working-experience/WorkingExperience.tsx
@@ -5,15 +5,16 @@ import {
   FlexColumnCenteredContainer,
   // Flex1,
 } from "../../components/styled-component/Container";
+// components
 import Cleanstormwater from "../../components/working-experience-component/clean-stormwater/CleanStormwater";
 import Fila from "../../components/working-experience-component/fila/Fila";
 import Funkdelics from "../../components/working-experience-component/funkdelics/Funkdelics";
+import Blackball from "../../components/working-experience-component/blackball/Blackball";
 
 export default function WorkingExperience() {
   return (
     <>
       <div style={{ padding: "20px 150px" }}>
-        <BlackCenterTitle>Present</BlackCenterTitle>
         <FlexColumnCenteredContainer style={{ gap: "10px" }}>
           <Cleanstormwater />
           <Fila />
@@ -22,9 +23,11 @@ export default function WorkingExperience() {
       </div>
       <BlackBackgroundContainer>
         <BlackCenterTitle style={{ color: "white" }}>
-          Previous Experience
+          Other Experiences
         </BlackCenterTitle>
-        <FlexColumnCenteredContainer>Coming soon!</FlexColumnCenteredContainer>
+        <FlexColumnCenteredContainer>
+          <Blackball />
+        </FlexColumnCenteredContainer>
       </BlackBackgroundContainer>
     </>
   );
