39a496174dab8885ba5e4b471dcb2db3a586a52f
Warrant TSAI <warrant@cleanstormwater.com.au>
2023-08-30 15:19:16 +1000

    feat: combine other experience to working experience

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import Layout from "./pages/layout/Layout";
 import AboutMe from "./pages/about-me/AboutMe";
 import LearningPath from "./pages/learning-path/LearningPath";
 import WorkingExperience from "./pages/working-experience/WorkingExperience";
-import OtherExperience from "./pages/other-experience/OtherExperience";
 import MyHobbies from "./pages/my-hobbies/MyHobbies";
 import ContactWithMe from "./pages/contact-with-me/ContactWithMe";
 import NotFoundPage from "./pages/not-found-page/NotFoundPage";
@@ -30,7 +29,6 @@ function App() {
         <Route path="about-me" element={<AboutMe />} />
         <Route path="learning-path" element={<LearningPath />} />
         <Route path="working-experience" element={<WorkingExperience />} />
-        <Route path="other-experience" element={<OtherExperience />} />
         <Route path="my-hobbies" element={<MyHobbies />} />
         <Route path="contact-with-me" element={<ContactWithMe />} />
         {/* Laboratory routes */}
