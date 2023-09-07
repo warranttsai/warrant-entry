1fe27f60713c3a97aaaee7695bd15fc43c48b6e8
Warrant TSAI <s3881314@student.rmit.edu.au>
2023-09-07 12:25:57 +1000

    update: add placeholder for about this web app page

--- src/pages/lab/about-this-web-app/AboutThisWebApp.tsx
+++ src/pages/lab/about-this-web-app/AboutThisWebApp.tsx
@@ -2,11 +2,18 @@ import { BlackCenterTitle } from "../../../components/styled-component/Title";
 
 export default function AboutThisWebApp() {
   return (
-    <>
+    <div>
       <BlackCenterTitle>
         This page was about the details and technology utizization of this web
         app.
       </BlackCenterTitle>
-    </>
+      <div>Technologies?</div>
+      <div>Modules?</div>
+      <div>File structure?</div>
+      <div>Git Repository Link?</div>
+      <div>What platform was it using?</div>
+      <div>Commits? link?</div>
+      <div>Who contributed to this project?</div>
+    </div>
   );
 }
