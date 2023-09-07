58cb44266fd13ce8ffc29ba24e63530169bd09a4
Warrant TSAI <s3881314@student.rmit.edu.au>
2023-09-07 23:23:19 +1000

    feat: add comment section component

---
+++ src/pages/contact-with-me/comment-section/CommentSection.tsx
@@ -0,0 +1,9 @@
+import { FlexColumnCenteredContainer } from "../../../components/styled-component/Container";
+
+export default function CommentSection() {
+  return (
+    <>
+      <FlexColumnCenteredContainer>123</FlexColumnCenteredContainer>
+    </>
+  );
+}
