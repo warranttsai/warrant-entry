10b588b51abbcb2b5d9496b785cd26c635a006e6
Warrant TSAI <s3881314@student.rmit.edu.au>
2023-09-19 09:33:22 +1000

    refactor: adjust lab layout

--- src/pages/lab/Lab.tsx
+++ src/pages/lab/Lab.tsx
@@ -2,16 +2,8 @@
 import { useNavigate } from "react-router-dom";
 import { Button, Divider } from "@mui/material";
 // styled-components
-import {
-  BlackCenterTitle,
-  WhiteCenterTitle,
-} from "../../components/styled-component/Title";
-import { BlackBackgroundContainer } from "../../components/styled-component/Container";
-import { ObjectOrientation } from "../../components/software-engineer-note/ObjectOrientation";
-import {
-  DrawImageForFrontEndDesign,
-  EncapsulateYourStyleSheet,
-} from "../../components/software-engineer-note/FrontEndDesign";
+import { BlackCenterTitle } from "../../components/styled-component/Title";
+import { LabSection } from "../../components/styled-component/Container";
 
 export default function Lab() {
   const navigate = useNavigate();
@@ -19,13 +11,27 @@ export default function Lab() {
   return (
     <>
       {/* About this web application */}
-      <div style={{ marginBlock: 10 }}>
+      <LabSection>
         <Button onClick={() => navigate("/lab/about-this-web-app")}>
           <BlackCenterTitle style={{ fontSize: 30 }}>
             📚 Something About This Web Application 📚
           </BlackCenterTitle>
         </Button>
-      </div>
+      </LabSection>
+      <Divider
+        style={{
+          width: "90%",
+          marginInline: "auto",
+        }}
+      />
+      {/* My Coding Bible */}
+      <LabSection>
+        <Button onClick={() => navigate("/lab/my-coding-bible")}>
+          <BlackCenterTitle style={{ fontSize: 30 }}>
+            🧑‍💻 My Coding Bible
+          </BlackCenterTitle>
+        </Button>
+      </LabSection>
       <Divider
         style={{
           width: "90%",
@@ -33,7 +39,7 @@ export default function Lab() {
         }}
       />
       {/* React Challenges */}
-      <div>
+      <LabSection>
         <a
           href="https://www.codevertiser.com/free-reactjs-coding-challenges-for-responsive-UI/"
           target="_blank"
@@ -52,27 +58,7 @@ export default function Lab() {
         <Button onClick={() => navigate("/lab/react-challenge-4")}>
           challenge #4
         </Button>
-      </div>
-      <br />
-      <br />
-      {/* Software Engineer Note */}
-      <BlackBackgroundContainer>
-        <WhiteCenterTitle>Software Engineer Note</WhiteCenterTitle>
-        <div style={{ paddingInline: 200 }}>
-          <span>
-            This section is my note for software engineer experience. Creating a
-            simple documentation here as my engineering bible.
-            <p style={{ color: "red" }}>
-              Note: These are all from my understanding. It might be slightly
-              different from general definition.{" "}
-            </p>
-          </span>
-        </div>
-        <br />
-        <DrawImageForFrontEndDesign />
-        <EncapsulateYourStyleSheet />
-        <ObjectOrientation />
-      </BlackBackgroundContainer>
+      </LabSection>
     </>
   );
 }
