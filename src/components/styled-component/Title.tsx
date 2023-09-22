cf3406e82b87f0a5e71023c4b5415541c8038b69
warranttsai <warrant1997@gmail.com>
2023-09-23 01:35:50 +1000

    fix: change min-width of container to 350px

--- src/components/styled-component/Title.tsx
+++ src/components/styled-component/Title.tsx
@@ -3,12 +3,11 @@ import styled from "styled-components";
 export const BrownCenterTitle = styled.h1`
   color: #b48608;
   font-family: "Droid serif", serif;
-  font-size: 60px;
+  font-size: 2.5 rem;
   font-weight: 400;
   font-style: italic;
   line-height: 44px;
   text-align: center;
-  min-width: 400px;
 `;
 
 export const BlackCenterTitle = styled.h1`
