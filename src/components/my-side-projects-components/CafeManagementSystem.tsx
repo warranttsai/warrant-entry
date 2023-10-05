b0e0d6c4e8f83a6b91e761980503163437c07ccf
Warrant TSAI <warrant1997@gmail.com>
2023-10-06 10:46:46 +1100

    feat: set up layout of CafeManagementSystem

--- src/components/my-side-projects-components/CafeManagementSystem.tsx
+++ src/components/my-side-projects-components/CafeManagementSystem.tsx
@@ -1,7 +1,27 @@
+import {
+  FlexColumnCenteredContainer,
+  Flex1,
+} from "../styled-component/Container";
+
 export default function CafeManagemengSystem() {
   return (
     <>
-      <h1>123</h1>
+      <FlexColumnCenteredContainer>
+        <Flex1>
+          <h3>Cafe Management System (C#, .Net)</h3>
+        </Flex1>
+        <Flex1>
+          GitHub:{" "}
+          <a href="https://github.com/warranttsai/CafeManagementSystem">
+            Click Me
+          </a>
+        </Flex1>
+        <Flex1>
+          Status:
+          <span style={{ color: "#F6BE00" }}>Under developing</span>
+        </Flex1>
+        <Flex1>Created Date: 06/10/2023</Flex1>
+      </FlexColumnCenteredContainer>
     </>
   );
 }
